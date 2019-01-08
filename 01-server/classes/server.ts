470df29350f4a99ecee756f6b5180c8506aabbf1
etorres <etorres@idec.edu.mx>
2019-01-08 11:52:42 -0600

    22 - start socketIO config

--- 01-server/classes/server.ts
+++ 01-server/classes/server.ts
@@ -1,17 +1,32 @@
 import express from "express";
 import { SERVER_PORT } from "../global/environment";
+import socketIO from "socket.io";
+import http from "http";
 
 export class Server {
     public app: express.Application;
     public port: number;
+    public io: socketIO.Server;
+    private httpServer: http.Server;
 
     constructor() {
         this.app = express();
         this.port = SERVER_PORT;
+
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO(this.httpServer);
+    }
+
+    private escucharSocket() {
+      console.log("Escuchando sockets");
+
+      this.io.on("conection", client => {
+        console.log("conected new client");
+      });
     }
 
     public start( callback: () => void ) {
-        this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port, callback);
     }
 
 }
