3423d38113e7ed5665babea8dce8a843f0a2ac84
etorres <etorres@idec.edu.mx>
2019-01-08 08:33:36 -0600

    delete dist

--- 01-server/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./classes/server");
-const router_1 = require("./routes/router");
-const server = new server_1.Server();
-server.app.use("/", router_1.router);
-server.start(() => {
-    console.log(`Servidor corriendo en el puerto ${server.port}`);
-});
