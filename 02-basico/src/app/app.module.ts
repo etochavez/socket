71caf6ff0c1a5cb4772bc534345c70d121920892
etorres <etorres@idec.edu.mx>
2019-01-09 10:38:35 -0600

    29 - ui to emit message

--- 02-basico/src/app/app.module.ts
+++ 02-basico/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 // Sockets
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
@@ -8,14 +9,17 @@ const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { FooteComponent } from './components/foote/foote.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooteComponent
+    FooteComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     SocketIoModule.forRoot(config)
   ],
   providers: [],
