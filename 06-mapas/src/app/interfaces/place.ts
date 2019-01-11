f6a02b5510813a39fc71846006fe299b81f205ef
etorres <etorres@idec.edu.mx>
2019-01-11 10:31:07 -0600

    78 - show markers on map

---
+++ 06-mapas/src/app/interfaces/place.ts
@@ -0,0 +1,7 @@
+
+export interface Place {
+  id?: string;
+  nombre: string;
+  lat: number;
+  lng: number;
+}
--- 06-mapas/src/app/map/map.component.ts
+++ 06-mapas/src/app/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Place } from '../interfaces/place';
 
 @Component({
   selector: 'app-map',
@@ -9,6 +10,26 @@ export class MapComponent implements OnInit {
 
   @ViewChild('map') mapElement: ElementRef;
   map: google.maps.Map;
+
+  markers: google.maps.Marker[] = [];
+
+  places: Place[] = [
+    {
+      nombre: 'Udemy',
+      lat: 37.784679,
+      lng: -122.395936
+    },
+    {
+      nombre: 'Bahía de San Francisco',
+      lat: 37.798933,
+      lng: -122.377732
+    },
+    {
+      nombre: 'The Palace Hotel',
+      lat: 37.788578,
+      lng: -122.401745
+    }
+  ];
 
   constructor() { }
 
@@ -23,8 +44,22 @@ export class MapComponent implements OnInit {
       center: latLng,
       zoom: 13,
       mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
+    };
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOPtions);
+
+    this.places.forEach((place: Place, i: number) => this.addBookmark(place));
+  }
+
+  addBookmark(bookmark: Place) {
+    const latLng = new google.maps.LatLng(bookmark.lat, bookmark.lng);
+
+    const marker =  new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: latLng,
+      draggable: true
+    });
+    this.markers.push(marker);
   }
 
 }
