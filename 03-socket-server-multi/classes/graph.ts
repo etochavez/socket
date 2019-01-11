0af5cd62f8efa1f70fb56682c29b0be06701ff62
etorres <etochavez@gmail.com>
2019-01-10 18:25:01 -0600

    62 - Graph model

---
+++ 03-socket-server-multi/classes/graph.ts
@@ -0,0 +1,17 @@
+export class Graph {
+  private months: Array<string> = ["January", "February", "March", "April"];
+  private values: Array<number> = [1, 2, 3, 4];
+
+  constructor() {}
+
+  public getGraphData() {
+    return [{ data: this.values, label: "Sells" }];
+  }
+
+  public changeValue(month: string, value: number) {
+    month = month.toLowerCase().trim();
+    this.values[this.months.findIndex(e => e === month)] += value;
+
+    return this.getGraphData();
+  }
+}
